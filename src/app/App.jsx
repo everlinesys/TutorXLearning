03ad9fe4d497431def94916aebe78f0976f786a1
Azhar <azhar.subair.2@gmail.com>
2026-06-30 11:24:19 +0530

    first commit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,7 +4,10 @@ import InstallButton from "../public/components/InstallButton";
 
 export default function App() {
   return (
-    <>
+    <> <div className="fixed top-0 left-0 w-full h-[100vh]  z-99 animate-pulse" >
+      <p className="text-red-500 text-2xl justify-center h-full">Website Under Maintenace </p>
+    </div>
+
       <RouterProvider router={router} />
       <InstallButton />
     </>
